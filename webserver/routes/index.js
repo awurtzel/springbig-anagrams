fd5dcf4cc2cf6f40baaebf8338d36eaa24d72388
Adam Wurtzel <awurtzel@byteme.com>
2020-11-05 21:39:36 -0800

    Impliment anagrams endpoints w/ helper functions built-in and 4 tests for each

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -1,30 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// function compareWords(newWord, existingWord) {
-//   //order the letters and compare the new strings OR return an int based on subtracting each char from one word into the other
-//   if (newWord === existingWord) {
-//     return 0;
-//   }
-// }
+function orderLetters(word) {
+  return word.split('').sort().join('');
+}
+function compareWords(newWord, existingWord) {
+  //order the letters and compare the new strings OR return an int based on subtracting each char from one word into the other
+  return orderLetters(newWord) === orderLetters(existingWord);
+}
+function addNewWordToCorrectBucket(newWord, anagramsHeap) {
+  let addedFlag = false;
+  for(const currBucket of anagramsHeap) {
+    if(compareWords(newWord, currBucket[0])) {
+      addedFlag = true;
+      currBucket.push(newWord);
+    }
+  }
+  if(!addedFlag) {
+    anagramsHeap.push([newWord]);
+  }
+}
 
-router.get('/v1/anagrams', function(req, res, next) { //take in query params
-  const anagramsHeap = [['nothing', 'ThingOn'], ['hello']];
-  //run query params one at a time comparing to each existing "bucket" in anagramsHeap
-  //first take the word and lower-case it
-  //if the next word "fits" the first word in the current "bucket", add it to the bucket
-  // else move on to the next "bucket" and check again...
-  //if there are no "buckets" that "fit" the current word, add it as it's own bucket to the end of anagramsHeap
-  res.send({ 'anagrams (v1)': anagramsHeap });
+router.get('/v1/anagrams', function(req, res, next) {
+  const words = req.query.words.split(',');
+  if(words.length > 0 && words[0].length > 0) {
+    const anagramsHeap = [];
+    words.forEach(newWord => {
+      newWord = newWord.toLowerCase();
+      addNewWordToCorrectBucket(newWord, anagramsHeap);
+    });
+    res.send({ 'anagrams (v1)': anagramsHeap });
+  }
+  res.send();
 });
 
-router.get('/v2/anagrams', function(req, res, next) { //take in query params
-  const anagramsHeap = [['Boo', 'ooB'], ['fake', 'kafe'], ['Kafe'], ['pot']];
-  //run query params one at a time comparing to each existing "bucket" in anagramsHeap
-  //if the next word "fits" the first word in the current "bucket", add it to the bucket
-  // else move on to the next "bucket" and check again...
-  //if there are no "buckets" that "fit" the current word, add it as it's own bucket to the end of anagramsHeap
-  res.send({ 'anagrams (v2)': anagramsHeap });
+router.get('/v2/anagrams', function(req, res, next) {
+  const words = req.query.words.split(',');
+  if(words.length > 0 && words[0].length > 0) {
+    const anagramsHeap = [];
+    words.forEach(newWord => {
+      addNewWordToCorrectBucket(newWord, anagramsHeap);
+    });
+    res.send({ 'anagrams (v2)': anagramsHeap });
+  }
+  res.send();
 });
 
 module.exports = router;
